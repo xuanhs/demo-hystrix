b539219c087b670daca3403f5b469f26e0e352e1
xuanzjie <xuanzjie@163.com>
2020-05-19 23:24:41 +0800

    change

--- src/main/java/com/example/demo/DemoHystrixApplication.java
+++ src/main/java/com/example/demo/DemoHystrixApplication.java
@@ -2,16 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCircuitBreaker
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
 public class DemoHystrixApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoHystrixApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHystrixApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/demo/controller/TestServiceController.java
+++ src/main/java/com/example/demo/controller/TestServiceController.java
@@ -1,17 +1,20 @@
 package com.example.demo.controller;
 
 import com.example.demo.api.TestServerApi;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestServiceController implements TestServerApi {
     @Override
-    public String normal(Integer id) {
+    public String normal(@Validated Integer id) {
         return "成功:" + id;
     }
 
     @Override
-    public String timeOut(Integer id) {
+    public String timeOut(@Validated Integer id) {
+        int k  = 0;
+        int i = 10/k;
         return "成功:" + id;
     }
 }
